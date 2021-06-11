48678436070005c945e5ef69377250a18f4ff5dc
AlexisAlegria <alexisalegriav@gmail.com>
2021-06-11 01:38:16 +0000

    second commit with 1 change in index.html

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,6 +4,7 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 export function Home() {
 	return (
 		<div className="text-center mt-5">
